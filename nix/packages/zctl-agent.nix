b5690365418448da175472ad9cdd0b0d6916dc3d
Bouraoui Ochi <bouraoui@outlook.it>
2026-05-27 08:04:43 +0200

    fix(ci): use vendor dir for Nix build, ignore vendor in prettier

--- nix/packages/zctl-agent.nix
+++ nix/packages/zctl-agent.nix
@@ -4,7 +4,7 @@ buildGoModule {
   version = "0.0.1";
   src = ../../agents/go-agent;
   subPackages = [ "cmd/agent" ];
-  vendorHash = "sha256-0Qxw+MUYVgzgWB8vi3HBYtVXSq/btfh4ZfV/m1chNrA=";
+  vendorHash = null;
 
   postInstall = ''
     mv $out/bin/agent $out/bin/zctl-agent
